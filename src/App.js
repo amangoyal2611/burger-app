4ce12fc6e55a9c17a13e3b536f9ff4a65174d552
Aman Goyal <im.developer26@gmail.com>
2020-06-02 02:05:44 +0530

    Layout created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-
+import Layout from '../src/components/Layout/Layout'
 class App extends Component {
 
   render() {
     return (
-      <div>
-      </div>
+      <Layout> <p>Hello</p> </Layout>
     );
   }
 }
