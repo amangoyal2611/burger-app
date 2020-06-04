e1e5c2efb3b33d655a82073b7929cacb122d1cee
Aman Goyal <im.developer26@gmail.com>
2020-06-04 23:58:20 +0530

    layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Layout from '../src/components/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 class App extends Component {
 
   render() {
     return (
-      <Layout> <p>Hello</p> </Layout>
+      <Layout>
+        <BurgerBuilder></BurgerBuilder>
+      </Layout>
     );
   }
 }
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Aux from '../../hoc/Auxx';
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <p>Burger</p>
+                <p>Build control</p>
+           </Aux> 
+        );
+    }
+}
+
+export default BurgerBuilder;
