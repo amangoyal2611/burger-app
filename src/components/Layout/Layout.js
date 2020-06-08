95776ef8751685571f4cd75fdcd4109b97e45ca4
Aman Goyal <im.developer26@gmail.com>
2020-06-09 02:01:24 +0530

    sidebar added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Aux from "../../hoc/Auxx";
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const layout = (props) => {
     return (
         <Aux>
             <Toolbar></Toolbar>
+            <SideDrawer></SideDrawer>
             <div>Sidedrawer, Backdrop</div>
             <main className={classes.Content}>
                 {props.children}
