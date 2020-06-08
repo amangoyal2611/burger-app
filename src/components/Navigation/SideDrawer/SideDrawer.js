90d3a36220429403eb48f17af9af3a532aa7def1
Aman Goyal <im.developer26@gmail.com>
2020-06-09 02:17:04 +0530

    sidebar close functionality added

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,28 @@ import React from 'react'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
 import classes from './SideDrawer.module.css'
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxx'
 
-const sideDrawer = () => {
+const sideDrawer = (props) => {
+
+    let attachClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachClasses = [classes.SideDrawer, classes.Open]
+    }
+
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo></Logo>
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}></Backdrop>
+            <div className={attachClasses.join(" ")}>
+                <div className={classes.Logo}>
+                    <Logo></Logo>
+                </div>
+                <nav>
+                    <NavigationItems></NavigationItems>
+                </nav>
             </div>
-            <nav>
-                <NavigationItems></NavigationItems>
-            </nav>
-        </div>
+        </Aux>
     );
 };
 
