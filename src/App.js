5afbc8cad59f13100ebf42d90a0528eb75f3ada7
Aman Goyal <im.developer26@gmail.com>
2020-06-09 03:05:48 +0530

    performance improvement

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Layout from '../src/components/Layout/Layout'
+import Layout from '../src/containers/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 class App extends Component {
 
