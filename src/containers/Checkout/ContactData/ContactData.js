9048e56dad663ea27346e314cf8c5abc649e7cff
Aman Goyal <im.developer26@gmail.com>
2020-07-08 21:33:41 +0530

    reduct added in contact data and checkout

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
 import Button from '../../../components/UI/Button/Button'
 import classes from './ContactData.module.css'
 import instance from '../../../axios'
@@ -120,8 +121,8 @@ class ContactData extends Component {
         }
 
         const order = {
-            ingredients: this.props.ingredients,
-            price: +this.props.totalPrice,
+            ingredients: this.props.ings,
+            price: +this.props.price,
             orderData: formData
         }
         console.log(order.price)
@@ -199,4 +200,11 @@ class ContactData extends Component {
     }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice
+    }
+}
+
+export default connect(mapStateToProps)(ContactData);
