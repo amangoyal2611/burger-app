7e769a209468e70b146e527d1abfe2984c8bc0d4
Aman Goyal <im.developer26@gmail.com>
2020-07-11 00:24:25 +0530

    fetching order by redux

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -3,46 +3,47 @@ import React, { Component } from 'react'
 import Order from '../../components/Order/Order'
 import instance from '../../axios';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
+import * as actions from '../../store/actions/index'
+import { connect } from 'react-redux'
+import Spinner from '../../components/UI/Spinner/Spinner'
 
 class Orders extends Component {
 
-    state = {
-        orders: [],
-        loading: true
-    }
+
 
     componentDidMount() {
-        instance.get('orders.json')
-            .then((res) => {
-
-                let fetchedOrders = [];
-                for (let key in res.data) {
-                    fetchedOrders.push({
-                        ...res.data[key],
-                        id: key
-                    })
-                }
-                this.setState({ loading: false, orders: fetchedOrders })
-                console.log(this.state.orders)
-
-            })
-            .catch(e => {
-                this.setState({ loading: false })
-            });
+        this.props.onFetchOrders();
     }
 
     render() {
+
+        let orders = <Spinner />;
+        if (!this.props.loading) {
+            orders = this.props.orders.map(order => {
+                console.log(order.ingredients);
+                return <Order key={order.id} ingredients={order.ingredients} totalPrice={order.price}></Order>
+            })
+        }
+
         return (
             <div>
-                {
-                    this.state.orders.map(order => {
-                        console.log(order.ingredients);
-                        return <Order key={order.id} ingredients={order.ingredients} totalPrice={order.price}></Order>
-                    })
-                }
+                {orders}
             </div>
         );
     }
 }
 
-export default withErrorHandler(Orders, instance);
+const mapDispatchToProps = dispatch => {
+    return {
+        onFetchOrders: () => { dispatch(actions.fetchOrders()) }
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        orders: state.order.orders,
+        loading: state.order.loading
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, instance));
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -10,6 +10,26 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
 
+        case actionTypes.FETCH_ORDERS_START:
+            return {
+                ...state,
+                loading: true,
+
+            }
+        
+            case actionTypes.FETCH_ORDERS_SUCCESS:
+                return {
+                    ...state,
+                    loading: true,
+                    orders: action.orders
+            }
+        
+            case actionTypes.FETCH_ORDERS_FAIL:
+                return {
+                    ...state,
+                    loading: false,      
+                }
+
         case actionTypes.PURCHASE_INIT:
             return {
                 ...state,
